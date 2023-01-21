57916f85b6f738770e8695139b14509ce9f54fa0
SHOKDHAR <121552501+918088529@users.noreply.github.com>
2023-01-21 09:52:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/MdiskSearchBotV2.git /MdiskSearchBotV2
-cd /MdiskSearchBotV2
+git clone https://github.com/918088529/king-bot.git /king-bot
+cd /king-bot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
